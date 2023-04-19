5eced1d8dcba1e88db8e0448f9c5e2fc40a52ddc
CBVMAzureInterview <130216159+CBVMAzureInterview@users.noreply.github.com>
2023-04-19 09:09:50 +0100

    Update docker_build.sh

--- docker_build.sh
+++ docker_build.sh
@@ -2,7 +2,7 @@ BUILDID=$1
 USERNAME=$2
 PASSWORD=${3}
 
-docker login -u $USERNAME ${PASSWORD} interviewregistry
+docker login -u $USERNAME ${PASSWORD} interviewregistry.azurecr.io
 docker build -t bestappever .
-docker tag bestappever interviewregistry/bestappever:$BUILDID
-docker push interviewregistry/bestappever:$BUILDID
+docker tag bestappever interviewregistry.azurecr.io/bestappever:$BUILDID
+docker push interviewregistry.azurecr.io/bestappever:$BUILDID
