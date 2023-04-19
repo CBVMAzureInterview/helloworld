b60294b428adace676446ddc914e5279fecd8fd0
CBVMAzureInterview <130216159+CBVMAzureInterview@users.noreply.github.com>
2023-04-19 10:46:34 +0100

    Delete docker_run.sh

--- docker_run.sh
+++
@@ -1,7 +0,0 @@
-BUILDID=$1
-USERNAME=$2
-PASSWORD=${3}
-
-docker login -u $USERNAME ${PASSWORD} interviewregistry
-docker pull interviewregistry.azurecr.io/bestappever:$BUILDID
-docker run -d -p 3000:3000 interviewregistry.azurecr.io/bestappever:$BUILDID
