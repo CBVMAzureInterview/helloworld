0c1bc3ac793f9992c7bef419dc9edd15a101cca5
CBVMAzureInterview <130216159+CBVMAzureInterview@users.noreply.github.com>
2023-04-19 10:46:24 +0100

    Delete docker_build.sh

--- docker_build.sh
+++
@@ -1,8 +0,0 @@
-BUILDID=$1
-USERNAME=$2
-PASSWORD=${3}
-
-docker login -u $USERNAME ${PASSWORD} interviewregistry.azurecr.io
-docker build -t bestappever .
-docker tag bestappever interviewregistry.azurecr.io/bestappever:$BUILDID
-docker push interviewregistry.azurecr.io/bestappever:$BUILDID
